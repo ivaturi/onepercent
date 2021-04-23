7163c9b13a1f21a444742174a5e8d822d15d6d06
abhi <abhi@mailbox.org>
2021-04-23 22:12:18 +0200

    python-morsels: notes on my solution for tail

--- python/python-morsels/tail/tail.py
+++ python/python-morsels/tail/tail.py
@@ -1,9 +1,18 @@
 #! /usr/bin/env python
 
-def tail(sequence, tail_length):
+
+"""
+This solution works, but is sub-optimal, since it involves converting the entire
+generator in one go to a list. This fails hard if the input sequence is a 50gb file
+because it'll eat up memory
+"""
+def tail_mine(sequence, tail_length):
     """ Return last n elements from the input sequence """
     if tail_length <= 0:
         return []
     seq = list(sequence)
     l = len(seq)
-    return [seq[i] for i in range(l-tail_length, l)] if tail_length < l else seq
+    return seq[-tail_length:] if tail_length < l else seq
+
+
+tail = tail_mine
